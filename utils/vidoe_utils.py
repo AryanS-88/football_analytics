e214d225a42d0299f2cf47ab0091acc0ebd4ac85
AryanS-88 <ssingharyan70@gmail.com>
2025-05-02 08:47:43 +0530

    video read and save utilities

--- utils/vidoe_utils.py
+++ utils/vidoe_utils.py
@@ -1,4 +1,24 @@
 import cv2
 
-def read_video(path):
-    
+def read_video(video_path):
+    capture = cv2.VideoCapture(video_path)
+    frames = []
+    while True:
+        ret, frame = capture.read()
+        if not ret:
+            break
+        frames.append(frame)
+    capture.release()
+    return frames
+
+def save_video(output_video_frames, output_video_path):
+    if not output_video_frames:
+        raise ValueError("No frames to write.")
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    height, width = output_video_frames[0].shape[:2]
+    out = cv2.VideoWriter(output_video_path, fourcc, 24, (width, height))
+
+    for frame in output_video_frames:
+        out.write(frame)
+    out.release()
