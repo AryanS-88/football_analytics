3894ba55e8800689a0b41af7ab0bbe1c06429228
AryanS-88 <ssingharyan70@gmail.com>
2025-05-02 11:07:38 +0530

    tracking players

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from utils import read_video, save_video
+from trackers import tracker
+
 def main():
-    #read video 
-    video_frames =read_video("input_videos/08fd33_4.mp4")
+    # Read video 
+    video_frames = read_video("input_videos/08fd33_4.mp4")
 
-    #save video
-    save_video(video_frames,"output_videos/output_video.avi")
+    # Initialize tracker
+    my_tracker = tracker("models/best.pt")
+    tracks = my_tracker.get_object_tracks(video_frames)
 
-  
+    # Save video
+    save_video(video_frames, "output_videos/output_video.avi")
+
 if __name__ == "__main__":
     main()
---
+++ trackers/tracker.py
@@ -0,0 +1,36 @@
+from ultralytics import YOLO
+import supervision as sv
+class tracker:
+    def __init__(self,model_path):
+        self.model =YOLO(model_path)
+        self.tracker=sv.ByteTrack()
+    
+    def detect_frames(self,frames):
+        batch_size=20
+        detections=[]
+        for i in range(0,len(frames),batch_size):
+            detections_batch=self.model.predict(frames[i:i+batch_size],conf=0.1)
+            detections+=detections_batch
+          
+        return detections
+        
+    def get_object_tracks(self,frames):
+        detections=self.detect_frames(frames)
+
+        for frame_num,detection in enumerate(detections):
+            cls_names=detection.names 
+            cls_names_inv = {v:k for k,v in cls_names.items()}
+            print(cls_names_inv)
+
+            # convert the detection to supervision detection format 
+
+            detection_supervison=sv.Detections.from_ultralytics(detection)
+
+            #convert goalkeeper to player 
+            for object_ind,class_id in enumerate(detection_supervison.class_id):
+                if cls_names[class_id]=='goalkeeper':
+                    detection_supervison.class_id[object_ind]=cls_names_inv["player"]
+
+            print(detection_supervison)
+            break
+          
