8984ee0822d5586eb443d9e216d0bf5db1356274
AryanS-88 <ssingharyan70@gmail.com>
2025-05-03 23:24:12 +0530

    player color analysis

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from utils import read_video, save_video
 from trackers import tracker
+import cv2
+import os
 
 def main():
     # Read video 
@@ -10,6 +12,19 @@ def main():
     tracks = Tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path="stubs/track_stubs.pkl")
+
+
+    # # Save cropped images of players in the first frame
+    # for track_id, player in tracks['players'][0].items():
+    #     bbox = player["bbox"]  # Extract the actual bbox list
+    #     frame = video_frames[0]
+
+    #     # Convert to int to avoid slicing errors
+    #     x1, y1, x2, y2 = map(int, bbox)
+    #     cropped_image = frame[y1:y2, x1:x2]
+
+    #     # Save image
+    #     cv2.imwrite(f"output_videos/cropped_image.jpg", cropped_image)
 
     # Draw object tracks
     output_video_frames = Tracker.draw_annotation(video_frames, tracks)
